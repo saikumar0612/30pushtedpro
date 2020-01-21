5dd26505d5add61b0a55e132afb4fe4385a7e9e9
Saikumar <saikumar.basolutions@gmail.com>
2020-01-21 10:11:57 +0530

    Update protractor.conf.js

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -136,12 +136,12 @@ exports.config = {
             platform = caps.get('platform');
 
             testConfig = {
-                reportTitle: 'Add leaveEntitlement ,Edit leaveEntitlement ,search leaveEntitlement under Leave Configuartion module for Test Execution Report',
+                reportTitle: 'Add leaveEntitlement ,Edit leaveEntitle, under Leave Configuartion module for Test Execution Report',
                 // console.log('dir ', path.join(__dirname, '../../'));
                 //outputPath: 'F:/Jasmine/CRM/vendorcompany/reports',
                 outputPath: 'H:/tesingpro/30pushtedpro/e2e/reports',
-                outputFilename: 'leaveEntitlement Test Execution Report',
-                screenshotPath: 'H:/tesingpro/30pushtedpro/e2e/screenshots',
+                outputFilename: 'index',
+                screenshotPath: 'H:/tesingpro/30pushtedpro/reports/e2e/screenshots',
                 testBrowser: browserName,
                 browserVersion: browserVersion,
                 modifiedSuiteName: false,
